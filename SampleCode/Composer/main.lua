376f80bb6e20aeec1549d7b247ab25f879aed118
jacob nielsen <indkasse@gmail.com>
2014-05-13 20:44:12 +0200

    v.0.2.5

--- SampleCode/Composer/main.lua
+++ SampleCode/Composer/main.lua
@@ -16,9 +16,6 @@ composer.gotoScene( "scene1", "fade", 300 )
 -- you setup composer and call composer.gotoScene
 ---------------------------------------------------------------------------------
 
--- If iOS and no ads in the app then you should set the GA.iosIdentifierForVendor property to true.
-GA.iosIdentifierForVendor = false
-
 GA.isDebug = true
 GA.runInSimulator = true
 
